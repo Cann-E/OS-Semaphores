607103d5775b8fc31c10866e6611b2a858dfb848
Cann-E <cann0211@gmail.com>
2025-03-24 11:22:01 -0500

    initial commit

---
+++ mutex_semaphores_6.cpp
@@ -0,0 +1,83 @@
+//GOAL: given a string of numbers, increment any even digits in this number, and keep the odd digits the same, using mutex semaphores to do so.
+#include <pthread.h>
+#include <iostream>
+#include <string>
+
+#include <semaphore.h>
+
+using namespace std;
+
+struct infoFromMain
+{
+    int threadID; //e.g. thread 0, thread 1....
+    int digit; //e.g. 1234, first digit - 1, second digit - 2...
+};
+
+
+static pthread_mutex_t bsem;    // Mutex semaphore
+static pthread_cond_t waitTurn = PTHREAD_COND_INITIALIZER;  // Condition variable to control the turn
+static int turn = 0; // value to control which child thread enters the critical section
+
+
+void *printDigit(void *void_ptr)
+{
+    //GOAL: given a string of numbers, increment any even digits in this number, and keep the odd digits the same, using mutex semaphores to do so.
+    pthread_mutex_lock(&bsem);
+    struct infoFromMain* infrma = (struct infoFromMain*)void_ptr;
+
+    while(infrma->threadID != turn){
+        pthread_cond_wait(&waitTurn,&bsem);
+    }
+
+    if (infrma->digit % 2 ==0){
+        cout<<infrma->digit+1;
+    }
+    else{
+        cout<<infrma->digit;
+    }
+
+    
+
+    turn++;
+    pthread_mutex_unlock(&bsem);
+
+    pthread_mutex_lock(&bsem);
+    pthread_cond_broadcast(&waitTurn);
+    pthread_mutex_unlock(&bsem);
+    
+
+    return nullptr;
+}
+
+int main()
+{
+    std::string input;
+    std::cin >> input; //e.g. 1234
+    
+    int nThreads = input.size();// initialize the number of threads variable;
+    pthread_t *tid = new pthread_t[nThreads];
+    infoFromMain *arg = new infoFromMain[nThreads];
+    
+    pthread_mutex_init(&bsem,nullptr);   // Initializing bsem
+    
+    for(int i=0;i<nThreads;i++)
+    {
+        arg[i].threadID=i;
+        arg[i].digit=input[i]-'0';
+        
+    }
+    for (int i=0;i<nThreads; i++){
+        if(pthread_create(&tid[i],NULL,printDigit,(void *)&arg[i])){
+            cerr<<"error";
+        }
+    }
+    
+    for(int i=0;i<nThreads;i++)
+        pthread_join(tid[i],nullptr);
+   
+    if (arg != nullptr)
+        delete [] arg; 
+    if (tid != nullptr)
+        delete [] tid;
+    return 0;
+}
